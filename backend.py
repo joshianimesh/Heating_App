c51e9a9c1c031ff6deebfc8d829a09e2ceb3fd3d
russhacker <valerii.matveev@student.hamk.fi>
2021-11-19 12:31:31 +0300

    time functions, renaming

--- backend.py
+++ backend.py
@@ -6,12 +6,20 @@ import json
 
 class Inputs:
 
-    def Take_inputs(self, answer):
+    def Take_input_manual(self, answer): #function to turn on manually without time control
         i = 0
         if answer == ('yes' or 'Yes'):
             i=1
         else: i=0
         return(i)
+
+    def Take_input_time(self, tim): #probably implementation of time setting mode (not finished)
+        res=0
+        while (cur!= tim) or (cur>tim):
+            cur = GetCurTime()
+            time.sleep(10)
+            res = 1
+        return(res)
 
 class OperatingInputs:
     
@@ -55,9 +63,9 @@ class OperatingOutputs:
         print('heater=', heater)   
         return(heater)
 
-def OnButtonPress(): 
+def ManualTurnOn(): 
     question = Inputs() #creating an instance of Inputs class
-    K = question.Take_inputs(input('Do u want me hot, daddy?')) #returning the result of Take_inputs with our answer into K
+    K = question.Take_input_manual(input('Do u want me hot, daddy?')) #returning the result of Take_inputs with our answer into K
 
     decision = OperatingInputs() #instance
     D = decision.DecisionOnPassing(K) #result of DecisionOnPassing with K given
@@ -66,20 +74,27 @@ def OnButtonPress():
     heating.PassToDevice(D)
 
 
-def ProcessingTime():
-    t = time.localtime()
-    string_time = time.asctime(t)
-    only_time = string_time[11:19]
-    hour = int(string_time[11:13])
-    minutes = int(string_time[14:16])
-    seconds = int(string_time[17:19])
-    print(hour, minutes, seconds)
+def GetCurTime():
+    time=[]
+    t = time.localtime() #current time
+    string_time = time.asctime(t) #converting current time to the string 
+
+    time.append(int(string_time[11:13])) #current hour in integer
+    time.append(int(string_time[14:16])) #current minute in int
+    time.append(int(string_time[17:19]))
+    return(time)
+    
+def ManualPutTime():
+    reply = []
+    set_tim = int(input('When im gonna sleep, daddy?'))
+    reply.append(set_tim)
+    return(reply)
 
 def main():
     Forecast()
-    OnButtonPress()    
-    ProcessingTime()
-   
+    #ManualTurnOn()    
+    GetCurTime()
+    
     
 
 if __name__=='__main__':
